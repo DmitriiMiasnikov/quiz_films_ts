bf06c73e9dffa576242b8beae181c16b917903de
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-09-01 02:12:11 +0300

    test ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+type AddressType = {
+  country: null | string,
+  city: null | string,
+}
+const initialState = {
+  name: null as string | null,
+  age: null as number | null,
+  address: [] as Array<AddressType>
+}
+export type initialStateType = typeof initialState;
+const state: initialStateType = {
+  name: 'asda',
+  age: 22,
+  address: [{
+    country: 'asas',
+    city: 'asda'
+  }]
+}
+console.log(state);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
     </div>
   );
 }
